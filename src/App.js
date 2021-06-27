ba4c4253fe423285ef872810f5b6e6fe8279fa39
Tina Skov Olesen <38000034+tinaskovo@users.noreply.github.com>
2021-06-27 12:47:14 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataTable from './DataTable';
+import './App.css';
+
+const App = (props) => {
+  return (
+    <div className="container mt-3">
+      <DataTable initialRows={props.rows} locale="da" rowsPerPage={5} />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/DataTable/index.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+import Pagination from './Pagination'
+import Row from './Row'
+import Search from './Search'
+
+const DataTable = ({ initialRows, rowsPerPage = 40 }) => {
+  const [rows, setRows] = useState(initialRows);
+  const [currentPageNumber, setCurrentPageNumber] = useState(0);
+
+  const calculateTotalNumberOfPages =  (rows) => {
+    if (rowsPerPage === 0) return 0
+    return Math.ceil(rows.length / rowsPerPage)
+  }
+
+  const [totalNumberOfPages, setTotalNumberOfPages] = useState(calculateTotalNumberOfPages(initialRows));
+
+  const search = (event) => {
+    const text = event.target.value
+    let rowsFound = initialRows
+
+    if (text) {
+      rowsFound = initialRows.filter((row) =>
+        row.name1.toLowerCase().search(text.toLowerCase()) > -1 ||
+        (row.email && row.email.toLowerCase().search(text.toLowerCase()) > -1)
+      )
+    }
+
+    setRows(rowsFound)
+    setCurrentPageNumber(0)
+    setTotalNumberOfPages(calculateTotalNumberOfPages(rowsFound))
+  }
+
+  const changeToPageNumber = (pageNumber) => {
+    setCurrentPageNumber(pageNumber)
+  }
+
+  const rowsInPageNumber = (pageNumber) => {
+    const startIndex = pageNumber * rowsPerPage
+    return [startIndex, startIndex + rowsPerPage]
+  }
+
+  const rowsToRender = rows.map(row => <Row key={row.per_id} row={row} />).slice(...rowsInPageNumber(currentPageNumber))
+
+  return(
+    <div>
+      <Search onSearch={search.bind(this)} />
+      <table>
+        <tbody>
+          { rowsToRender }
+        </tbody>
+      </table>
+      <Pagination
+        currentPageNumber={currentPageNumber}
+        totalNumberOfPages={totalNumberOfPages}
+        onChange={changeToPageNumber.bind(this)} />
+    </div>
+  )
+}
+
+export default DataTable
